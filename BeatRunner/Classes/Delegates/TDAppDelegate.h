f6c328d4289574d2ecccb36d57d40c692ab91ea8
Tony DiPasquale <tony@thoughtbot.com>
2014-02-07 17:16:42 -0500

    Initial Commit

---
+++ BeatRunner/Classes/Delegates/TDAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  TDAppDelegate.h
+//  BeatRunner
+//
+//  Created by Tony DiPasquale on 2/7/14.
+//
+//
+
+@interface TDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BeatRunner/Classes/Delegates/TDAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  TDAppDelegate.m
+//  BeatRunner
+//
+//  Created by Tony DiPasquale on 2/7/14.
+//
+//
+
+#import "TDAppDelegate.h"
+
+@implementation TDAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    return YES;
+}
+
+@end
